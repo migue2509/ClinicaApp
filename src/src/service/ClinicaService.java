5e758daf3e97594219a621a93bd62642f28a583b
migue2509 <miguel.ospina.dev@gmail.com>
2026-08-24 10:41:09 -0500

    feat: Arquitectura y distribucion de responsabilidades

---
+++ src/src/service/ClinicaService.java
@@ -0,0 +1,34 @@
+package service;
+
+public class ClinicaService {
+    // ENCARGADO: MIGUEL TAREA: implementar Consultable y declarar las listas privadas
+
+
+
+
+
+
+
+
+    //ENCARGADA: MARTHA CARO TAREA: METODOS DE PACIENTE
+
+
+
+
+
+
+
+
+    // ENCARGADO: MICHAEL TAREA: METODOS DE MEDICO
+
+
+
+
+
+
+    // ENCARGADO: JHOHAN TAREA: METODOS DE TURNO
+
+
+
+
+}
